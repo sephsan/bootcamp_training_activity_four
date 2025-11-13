28a0d375f0fdb276f81e387c30a9743296aaf145
Joseph Bernell Agbing <satsvn_josephba@sats.com.sg>
2025-11-13 06:44:14 +0000

    implementation of requirement on MainView and Controller

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,10 +1,55 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], (Controller,MessageToast) => {
     "use strict";
 
     return Controller.extend("exer1agbing.controller.MainView", {
+        
         onInit() {
+        },
+        onAddItem: function (){
+               // this.fnDisplayMsg("Add button pressed");
+               var oTextBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+                var sMsg = oTextBundle.getText("addButtonMsg");
+                this.fnDisplayMsg(sMsg);
+
+            },
+        onChangeMOP: function (oEvent) {
+                var sSelectedKey = oEvent.getParameter("selectedItem").getProperty("key");
+                var oMobileLabel = this.getView().byId("idLblPhone");
+                var oMobileInput = this.getView().byId("idInputPhone");
+                var oCCLabel = this.getView().byId("idLblCC");
+                var oCCInput = this.getView().byId("idInputCC");
+                var oSelect = oEvent.getSource(); 
+                var sSelectedText =   oSelect.getSelectedItem().getText();
+                if (sSelectedKey === "GCASH"){
+                    // show the mobile field
+                    oMobileLabel.setVisible(true);
+                    oMobileInput.setVisible(true);
+                }
+                else if(sSelectedKey === "CC"){
+                    oCCLabel.setVisible(true);
+                    oCCInput.setVisible(true);
+                } else {
+                    oMobileLabel.setVisible(false);
+                    oMobileInput.setVisible(false);
+                     oCCLabel.setVisible(false);
+                    oCCInput.setVisible(false);
+                }
+              
+                this.fnDisplayMsg(sSelectedText);
+            },
+        onPressCheckout: function (){
+                var oInputFNameValue = this.getView().byId("idInptFName").getValue();
+
+                // Check if first name is blank
+                if (oInputFNameValue === ""){
+                    sap.m.MessageToast.show("Required Field is blank"); 
+                }
+            },
+        fnDisplayMsg: function (sMsg){
+            MessageToast.show(sMsg);
         }
     });
 });
